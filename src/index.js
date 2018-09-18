e1985e1e3564683b41ab2a2a9f48cdaf3ccd75eb
DasneiN <sank.mail@gmail.com>
2018-09-18 13:21:47 +0300

    First steps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+  //  Проверка, решен ли судоку
+  let isSolved = function () {
+    
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i].indexOf(0)) {
+        return false;
+      }
+    }
+
+    return true;
+    
+  }
+  
+  //  Функция проверки ряда
+  let checkRow = function (number, row) {
+    return matrix[row].indexOf(number) ? false : true;
+  }
+
+  //  Функция проверки столбца
+  let checkCol = function (number, col) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i][col] == number) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  
+
 }
