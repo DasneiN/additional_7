3ac9def60048a6f07f618027dd7faf0837669558
DasneiN <sank.mail@gmail.com>
2018-09-21 20:27:19 +0300

    Solution

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-module.exports = function solveSudoku(matrix) {
+/*module.exports = */function solveSudoku(matrix) {
   
   //  Проверка, решен ли судоку
   let isSolved = function () {
@@ -10,6 +10,7 @@ module.exports = function solveSudoku(matrix) {
     }
 
     return true;
+    
   }
 
   //  Составляем прогноз на ячейки
@@ -32,6 +33,7 @@ module.exports = function solveSudoku(matrix) {
     }
     
     matrix[row][col] = variants;
+    
   }
 
 
@@ -65,3 +67,17 @@ module.exports = function solveSudoku(matrix) {
   return matrix;
 
 }
+
+var initial = [
+  [5, 3, 4, 6, 7, 8, 9, 0, 0],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+solveSudoku(initial);
