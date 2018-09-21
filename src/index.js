4911b3f61b4a1c05c3752544b3dd0530df67fc3b
DasneiN <sank.mail@gmail.com>
2018-09-21 19:43:53 +0300

    Before new solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function solveSudoku(matrix) {
 
+  /*
   let squaresValues = [],
       squaresCoords = [];
 
@@ -38,12 +39,10 @@ module.exports = function solveSudoku(matrix) {
           arrCoords = [];
 
       for (let i = 0; i < 3; i++) {
-
         for (let j = 0; j < 3; j++) {
           arrValues.push(matrix[k * 3 + i][z*3+j]);
           arrCoords.push([(k * 3 + i), (z * 3 + j)]);
         }
-
       }
 
       squaresValues.push(arrValues);
@@ -52,10 +51,59 @@ module.exports = function solveSudoku(matrix) {
     }
   }
   
-  while (!isSolved) {
+  while (!isSolved()) {
 
-    
+    for (let i = 0; i < squaresValues.length; i++) {
+
+      //  Find numbers that's not identified
+      let notIdentifiedNumbers = [];
+      for (let z = 1; z <= 9; z++) {
+        if (squaresValues[i].indexOf(z) < 0) {
+          notIdentifiedNumbers.push(z);
+        }
+      }
+      console.log(notIdentifiedNumbers);
+      
+
+      //  Find variants
+      for (let k = 0; k < notIdentifiedNumbers.length; k++) {
+
+        let arr = [],
+            arrCoords = [];
+        
+        for (let j = 0; j < squaresValues[i].length; j++) {
+          if (squaresValues[i][j] == 0) {
+            arr.push((checkRow(notIdentifiedNumbers[k], squaresCoords[i][j][0]) && checkCol(notIdentifiedNumbers[k], squaresCoords[i][j][1]) ) );
+            arrCoords.push(squaresCoords[i][j]);
+          }
+        }
+
+        if (arr.indexOf(true) == arr.lastIndexOf(true)) {
+          matrix[squaresCoords[i][arr.indexOf(true)][0]][squaresCoords[i][arr.indexOf(true)][1]] = notIdentifiedNumbers[k];
+        }
+        
+      }
+
+    }
     
   }
+
+  console.log(matrix);
+  */
+  
+  // return matrix;
+
+  let res = [];
+  for (let j = 1; j <= 9; j++) {
+    let arr = [];
+    for (let i = 0; i < 9; i++) {
+      arr.push((i + j) % 10);
+    }
+    res.push(arr);
+  }
+
+  console.log(res);
+  
+  return res;
 
 }
